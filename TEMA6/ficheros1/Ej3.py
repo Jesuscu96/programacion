983b68c38d61d9c5b3ac452f15406827acde9377
Jesuscu96 <jclementeuroz@gmail.com>
2025-02-12 22:24:49 +0100

    12-02-fichero1-Ej3

--- TEMA6/ficheros1/Ej3.py
+++ TEMA6/ficheros1/Ej3.py
@@ -1,24 +1,24 @@
 try:
-        Ej3 = open("datos_2.txt","r+")
-        while True:
-            print("\n--- Menú de combate ---")
-            print("1. introduce un nombre y edad")
-            print("2. Salir")
+    Ej3 = open("datos_2.txt","r+")
+    while True:
+        print("\n--- Menú de combate ---")
+        print("1. introduce un nombre y edad")
+        print("2. Salir")
 
-            opcion = input("Elige una opción: ")
-            
-            match opcion:
-                case "1":            
-                    nombre = str(input("introduce un nombre: "))
-                    edad = int(input("introduce la edad: "))
-                case "2":
-                    print("Fin del programa")
-                    Ej2.close
-                    break
-                case _:
-                    print("introduce de nuevo la opciones que son 1 o 2.")
+        opcion = input("Elige una opción: ")
         
-            Ej3.write(f"nombre: {nombre}, edad: {edad}. \n")
+        match opcion:
+            case "1":            
+                nombre = str(input("introduce un nombre: "))
+                edad = int(input("introduce la edad: "))
+            case "2":
+                print("Fin del programa")
+                Ej3.close()
+                break
+            case _:
+                print("introduce de nuevo la opciones que son 1 o 2.")
+    
+        Ej3.write(f"nombre: {nombre}, edad: {edad}. \n")
 except ValueError:
     print("Error al introducir un str en un int")
 finally:
