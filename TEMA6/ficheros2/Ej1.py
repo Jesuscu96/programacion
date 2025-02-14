b66187da533a3739ffbe43f58bc7636a030f6e79
Jesuscu96 <jclementeuroz@gmail.com>
2025-02-14 07:35:39 +0100

    ficheros 2

---
+++ TEMA6/ficheros2/Ej1.py
@@ -0,0 +1,49 @@
+
+
+while True:
+    print("\n--- Menú de combate ---")
+    print("1. Introduce nueva fecha (dd/mm/yyyy) con sus temperaturas.")
+    print("2. Mostrar historial de registros.")
+    print("3. Salir.")
+    
+    
+    Ej1= open("Temperatura.txt","r+" ,encoding = "utf-8")
+   
+    try:
+        opcion = input("Elige una opción: ")
+        
+        match opcion:
+            case "1":            
+                fecha = str(input("Introduce la fecha: "))
+                tmax = int(input("Introduce la temperatura maxima: "))
+                tmin = int(input("Introduce la temperatura minima: "))
+                Ej1.write(f"Fecha: {fecha}, Temperatura máxima: {tmax}, Temperatura mínima: {tmin}. \n")
+            case "2":
+                if Ej1:
+                    print("Mostrar registros: ")
+                    print(Ej1.read())
+                    temperaturaMax = 0
+                    temperaturaMin = 0 
+                    if temperaturaMax < tmax:
+                        temperaturaMax = tmax
+                    else: 
+                        temperaturaMax = temperaturaMax
+                    if temperaturaMin > tmin:
+                        temperaturaMin = tmin 
+                    else:
+                        temperaturaMin = tmin
+                        print(f"La tempeeatura mínima es de {temperaturaMin}ºc y la temperatura maxima es de {temperaturaMax}ºc")
+            case "3":
+                print("Fin del programa")
+                Ej1.close()
+                break
+            case _:
+                print("introduce de nuevo la opciones que son 1, 2 o 3.")
+        
+        
+    except ValueError:
+        print("Has introducido un tipo str en un int")
+    except FileNotFoundError:
+        print("El archivo no existe.")
+    finally:
+        Ej1.close()
---
+++ TEMA6/ficheros2/Ej2.py
@@ -0,0 +1,41 @@
+while True:
+    print("\n--- Menú de combate ---")
+    print("1. Introduce un nuevo contacto.")
+    print("2. Buscar por nombre.")
+    print("3. Mostrar datos.")
+    print("4. Guadar en fichiero.")
+    print("5. borrar contacto.")
+    print("6. Salir.")
+    
+    
+    Ej2= open("agenda.txt","r+" ,encoding = "utf-8")
+   
+    try:
+        opcion = input("Elige una opción: ")
+        
+        match opcion:
+            case "1":
+                for i in range (20):            
+                    nombre = str(input("Introduce un nombre: "))
+                    tlf = int(input("Introduce el numero de teléfono: "))
+                
+                Ej2.write(f"Nombre: {nombre}, Numero de teléfono: {tlf}. \n")
+            case "3":
+                if Ej2:
+                    print("Mostrar registros: ")
+                    print(Ej2.read())
+                 
+            case "6":
+                print("Fin del programa")
+                Ej2.close()
+                break
+            case _:
+                print("introduce de nuevo la opciones que son 1, ... o 6.")
+        
+        
+    except ValueError:
+        print("Has introducido un tipo str en un int")
+    except FileNotFoundError:
+        print("El archivo no existe.")
+    finally:
+        Ej2.close()
